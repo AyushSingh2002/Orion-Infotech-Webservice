373e82d6d565eebb30dc918581677e029bb87677
Ayush Singh <ayushhsingh2002@gmail.com>
2024-08-21 22:04:16 +0530

    Add Pages and Routes to App.jsx

--- orion-client/src/App.jsx
+++ orion-client/src/App.jsx
@@ -1,9 +1,17 @@
-import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import Contact from "./pages/Contact";
 export default function App() {
   return (
-    <>
-      <h1 className="text-red-500">ORION INFOTECH</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
